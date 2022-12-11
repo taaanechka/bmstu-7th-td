dbeac3c5add0a023ec65eafb86ebb5ebfddc27b3
Suchkova Tatiana <tanya18112001@mail.ru>
2022-12-11 20:30:05 +0300

    [3] Benchmark: upd run.sh. EmbedIO

--- Testing/Benchmark/Scripts/run.sh
+++ Testing/Benchmark/Scripts/run.sh
@@ -30,3 +30,32 @@ echo "Stop Carter container in docker."
 docker-compose -f dockerCarter/docker-compose.yml down
 echo
 
+echo "Start EmbedIO container in docker."
+docker-compose -f dockerEmbedIO/docker-compose.yml up -d
+echo
+
+echo "EmbedIO Benchmark has started!"
+echo
+
+echo "ab -n 1000 -c 100 http://localhost/api/json"
+ab -n 1000 -c 100 http://localhost/api/json > "report/embedio_json.csv"
+echo
+
+echo "ab -n 1000 -c 100 http://localhost/api/plaintext"
+ab -n 1000 -c 100 http://localhost/api/plaintext > "report/embedio_plaintext.csv"
+echo
+
+echo "ab -n 1000 -c 100 http://localhost/api/middle"
+ab -n 1000 -c 100 http://localhost/api/middle > "report/embedio_middle.csv"
+echo
+
+echo "ab -n 1000 -c 100 http://localhost/api/heavy"
+ab -n 1000 -c 100 http://localhost/api/heavy > "report/embedio_heavy.csv"
+echo
+
+echo "EmbedIO Benchmark has finished!"
+echo
+
+echo "Stop EmbedIO container in docker."
+docker-compose -f dockerEmbedIO/docker-compose.yml down
+echo
